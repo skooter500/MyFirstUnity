f1286c8732d70162325d28b0cbc219c7e6cfa854
skooter500 <bryan.duggan@tudublin.ie>
2024-03-14 15:59:21 +0000

    bla

---
+++ Assets/Tank.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * moveVertical * speed * Time.deltaTime);
+    }
+}
